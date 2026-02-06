bba2c046d7cc955ffefe72da963d22e730767555
rajesh123rai <rajes1173@gmail.com>
2026-02-07 00:32:46 +0530

    Push

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
-    public static void main(String[] args) {
-        System.out.println("hello java");
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello java";
     }
 }
